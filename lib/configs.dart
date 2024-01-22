8b7142f61d411c433294a6117cb7e9c239a3f12a
Hashir Awan <malikhashiraliawan@gmail.com>
2024-01-22 18:34:49 +0500

    logo and icon changed

--- lib/configs.dart
+++ lib/configs.dart
@@ -4,7 +4,7 @@ import 'package:country_picker/country_picker.dart';
 const APP_NAME = "divegram";
 
 /// App Icon src
-const APP_ICON = "assets/app_icon.png";
+const   APP_ICON = "assets/logo.png";
 
 /// Splash screen image src
 const SPLASH_SCREEN_IMAGE = 'assets/images/splash_image.png';
